a254da0436af3d8c75351c5778f2f558bf2a86e0
emanoeldelfino <emanoeldelfno@gmail.com>
2022-09-18 23:46:41 -0300

    Remove unused files

--- .vscode/setup.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-# Save to file
-# code --list-extensions > extensions.list
-
-# Install from file
-# cat extensions.list | xargs -L1 code --install-extension
-
--- README.md
+++
@@ -1,2 +0,0 @@
-source <(curl -s https://github.com/emanoeldelfino/dotfiles/blob/main/setup.sh)
-
